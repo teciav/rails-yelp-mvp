cab163699c55bbfb99e04d230f47a7a926803f05
Tecia Vailati <teciavailati@gmail.com>
2019-05-15 17:09:59 +0200

    restaurant and reviews page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create(name: 'Bobom', address: 'Breestraat 52', phone_number: '852-3596', category: 'chinese')
+
+Restaurant.create(name: 'Luma', address: 'Lumastraat 52', phone_number: '852-6396', category: 'french')
+
+Restaurant.create(name: 'Mona', address: 'Monastraat 52', phone_number: '522-3596', category: 'french')
+
+Restaurant.create(name: 'JapanM', address: 'Tokyostraat 52', phone_number: '852-3574', category: 'japanese')
+
+Restaurant.create(name: 'Alessandro', address: 'Italiastraat 52', phone_number: '852-3541', category: 'italian')
+
+Restaurant.create(name: 'Central Perk', address: 'Madison square 53', phone_number: '852-3226', category: 'belgian')
